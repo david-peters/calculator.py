8e3b03b61b889db67f9d410f0b360c918f5264d2
david-peters <davepeters89@gmail.com>
2020-03-01 00:28:12 +0000

    Add files via upload
    this has the power multiplied and the power divided included in the update file

--- calculator.py
+++ calculator.py
@@ -13,19 +13,21 @@ def divide(x, y):
     return x / y
 def subtract(x, y):
    return x - y
-def power(x, y,):
+def power(x, y):
     return x ** y
-def sin(a):
-    a = math.pi/6
-    return a
+def multiplyPower (x, y, z):
+    return x ** y * z
+def dividePower (x, y, z):
+    return x ** y / z
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 print("5.power of")
-print("6. sin")
-print("7. cos")
+print("6. multiplaying number to power")
+print("7. dividing a number to power")
+
 
 
 # Take input from the user
@@ -33,7 +35,6 @@ choice = input("Enter choice(1/2/3/4/5/6/7): ")
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-
 
 if choice == '1':
    print(num1,"+",num2,"=", add(num1,num2))
@@ -48,6 +49,13 @@ elif choice == '4':
    print(num1,"/",num2,"=", divide(num1,num2))
 elif choice == '5':
     print(num1, "**",num2, "=", power(num1,num2))
+elif choice == '6':
+    num3 = float(input("Enter third number for multiplying it to the power"))
+    print(num1,"**",num2, "*", num3, "=", multiplyPower(num1, num2, num3))
+elif choice == '7':
+    num3 = float(input("Enter third number for multiplying it to the power"))
+    print(num1,"**",num2, "*", num3, "=", dividePower(num1, num2, num3))
+
 
 else:
    print("Invalid input")
